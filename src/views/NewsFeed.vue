f981880e13fd65f9645639b14b3d73a6d23a4cbc
Maurice Griffin <mauricegriffin@gmail.com>
2019-04-17 13:43:24 -0700

    trying some article styles

--- src/views/NewsFeed.vue
+++ src/views/NewsFeed.vue
@@ -5,16 +5,21 @@
             Loading
         </template>
         <template row wrap v-if="!loading">
-            <article v-for="(item, index) in orderedRss" v-bind:key="item.link">
+            <article v-for="(item, index) in orderedRss" v-bind:key="item.link" v-bind:class="{ 'has-image': item.img }">
+                <div class="article-img">
+                    <img v-if="item.img" :src="item.img" alt="Image from article" />
+                </div>
+
                 <header>
                     {{item.title}}
                 </header>
                 <p class="snippet" v-html="item.contentSnippet"></p>
-                <v-img class="article-img" v-if="item.img" :src="item.img" alt=""></v-img>
+
                 <!-- <aside>
                 </aside> -->
                 <footer>
-                    {{item.source}} / {{item.postAge}} <template v-if="item.creator">by {{item.creator}}</template>
+                    {{item.source}} / {{item.postAge}} 
+                    <!-- <template v-if="item.creator"> / by {{item.creator}}</template> -->
                 </footer>
             </article>
         </template>
@@ -95,6 +100,9 @@ export default {
     created: function() {
         // this.$store.dispatch('getRssResults');
         // this.getRssResults();
+        if (!this.orderedRss.length) {
+            this.getRssResults();
+        }
     },
     beforeRouteEnter (to, from, next) {
         // console.log(to, from, next)
@@ -118,24 +126,101 @@ $greyBrown: #5a5353;
 $avocado: #ade25d;
 $dust: #a0a083;
 $teal: #3b7080;
+$blackish: #1E1C1C;
 // ----------------
 
 .news {
+    padding: 1.5em 1em;
     article {
-        background-color: $tan;
+        // background-color: lighten($dust,20%);
+        color: $blackish;
+        color: #fff;
+        margin-bottom: 1em;
+        clear:both;
+        // display: grid;
+        // grid-template-columns: 40px 50px auto 50px 40px;
+        // grid-template-rows: auto auto auto;
     }
     header {
         // background-color:  var(--v-secondary-base);
-        background-color: $greyBrown;
+        // background-color: $greyBrown;
+        font-size: 1.5rem;
+        line-height: 1.75rem;
+        font-family: $narrow;
+                        // border-bottom: 1px solid $avocado;
+
+        color: $avocado;
+        // background-color: $greyBrown;
+        // margin-bottom: 0.5em;
+        // border-bottom: 1px solid $avocado;
+        // padding-bottom: .5rem;
     }
     .snippet {
-
+        // color: $blackish;
+        // display: inline-block;
     }
     footer {
-
+        text-align: right;
+        // background-color: $avocado;
+        // color: $blackish;
+        // clear: both;
+        font-size: .8rem;
+        clear: both;
+        color: $avocado;
+    }
+    .has-image {
+        header {
+            // padding-left: 1rem;
+            // margin-left: 1rem;
+        }
     }
     .article-img {
+        float: left;
+        margin-right: 1rem;
 
+        // width: 25%;
+        // max-width: 25%;
+        // height: 100%;
+        img {
+            // width: 25%;
+            height: 9rem;
+            width: 9rem;
+            max-width: 100%;
+            object-fit: cover;
+        }
+        // width: 100%;
+        // max-width: 25%;
+    }   
+    /* styles for '...' */ 
+    .snippet {
+
+// padding-bottom: 5rem;
+        padding-top: 0.5rem;
+        margin-bottom: 0;
+    /* hide text if it more than N lines  */
+    overflow: hidden;
+    /* for set '...' in absolute position */
+    position: relative; 
+    /* use this value to count block height */
+    line-height: 1.2em;
+    /* max-height = line-height (1.2) * lines max number (3) */
+    // max-height: 4.8em;
+    max-height: 5.6rem;
+    /* fix problem when last visible word doesn't adjoin right side  */
+    // text-align: justify;  
+    /* place for '...' */
+    // margin-right: -1em;
+    // padding-right: 1em;
+    }
+    /* create the ... */
+    .snippet:before {
+        content:'';
+        width:100%;
+        height:100%;    
+        position:absolute;
+        left:0;
+        top:0;
+        background:linear-gradient(transparent 70%, $background);
     }
 }
 </style>
